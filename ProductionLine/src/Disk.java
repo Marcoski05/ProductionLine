7624be638348d66442e608b44b12bc11b19871ef
Marcoski05 <marco.crivas24@bishopireton.org>
2023-02-19 22:51:59 -0500

    Added Disk

---
+++ ProductionLine/src/Disk.java
@@ -0,0 +1,25 @@
+/**
+ * @author mcecc
+ *
+ */
+public class Disk implements Comparable<Disk> {
+   
+   int radius;
+   
+   public Disk (int r) {
+      radius = r;
+   }
+   
+   @Override
+   public int compareTo(Disk other) {
+      return radius - other.radius;
+   }
+   
+   @Override
+   public boolean equals(Object other) {
+      if (other instanceof Disk)
+         return this.compareTo((Disk)other) == 0;
+      
+      return false;
+   }
+}
