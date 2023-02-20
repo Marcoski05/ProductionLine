66ab50755edf34a364f88e083850da2b42009351
Marcoski05 <marco.crivas24@bishopireton.org>
2023-02-19 22:28:58 -0500

    Setup

---
+++ com.mcecc.git.first/src/Disk.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author mcecc
+ *
+ */
+public class Disk implements Comparable<Disk> {
+
+	@Override
+	public int compareTo(Disk o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ com.mcecc.git.first/src/module-info.java
@@ -0,0 +1,2 @@
+module com.mcecc.git.first {
+}
